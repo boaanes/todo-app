88f5f10223a1c80aa40f679227db35255b745732
Bo Aanes <boaanes@gmail.com>
2020-02-26 16:47:04 +0100

    :sparkles: Footer
    added footer with github and mail link

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './app.scss';
 
 import MainContainer from './components/MainContainer/MainContainer';
+import Footer from './components/Footer/Footer'
 
 const App = () => (
     <div className="container">
         <MainContainer />
+        <Footer />
     </div>
 )
 
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Footer = () => (
+        <div className="footer">
+            <div className="logo-container">
+                <img
+                    className="github-logo"
+                    src={require('../../assets/github-logo.png')}
+                    onClick={() => window.open("https://github.com/boaanes/todo-app/", '_blank').focus()}
+                    alt="Github Link"
+                />
+                <img
+                    className="mail-logo"
+                    src={require('../../assets/mail-logo.png')}
+                    onClick={() => window.open("mailto:boaanes@gmail.com", '_blank').focus()}
+                    alt="Send me a mail!"
+                />
+            </div>
+            <div className="signature">
+                <p>Bo Aanes - 2019</p>
+            </div>
+        </div>
+);
+
+export default Footer;

2ce1abab085b4ea2ffa7718d54445ab52666298f
Bo Aanes <boaanes@gmail.com>
2020-05-17 00:12:09 +0200

    :chart: Removed google analytics
    Google tag manager breaks login in brave browser

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,7 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
-import 'firebase/analytics';
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -10,11 +9,9 @@ const firebaseConfig = {
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+    appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 
 export default firebase;

a7ac140330fdc58bcbc4ea442fa602272490743f
Bo Aanes <boaanes@gmail.com>
2020-05-19 21:40:53 +0200

    :ambulance: Environment

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,6 +3,9 @@ import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/analytics';
 
+require('dotenv').config();
+console.log(process.env);
+
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
